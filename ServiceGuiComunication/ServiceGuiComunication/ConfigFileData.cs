5331f9c4d1324464c2783cabeefa71763ed21be0
Yuval Weinstein <yuvalw11@Yuvals-MacBook-Pro.local>
2018-05-07 19:22:39 +0300

    comunication proj update

---
+++ ServiceGuiComunication/ServiceGuiComunication/ConfigFileData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceGuiComunication
+{
+    public struct ConfigFileData
+    {
+        public string OutputDir { get; set; }
+        public string SourceDir { get; set; }
+        public string LogName { get; set; }
+        public string ThumnailSize { get; set; }
+        public string[] InputDirs { get; set; }
+    }
+}
---
+++ ServiceGuiComunication/ServiceGuiComunication/JsonConvertor.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceGuiComunication
+{
+    public class JsonConvertor
+    {
+        public static string ConvertToJson(ConfigFileData cfd)
+        {
+            JObject json = new JObject();
+            json["outputDir"] = cfd.OutputDir;
+            json["sourceDir"] = cfd.SourceDir;
+            json["logName"] = cfd.LogName;
+            json["thumnailSize"] = cfd.ThumnailSize;
+            json["inputDirs"] = new JArray(cfd.InputDirs);
+
+            return json.ToString();
+        }
+        
+        public static ConfigFileData ConvertToConfigFileData(string json)
+        {
+            JObject jsonObj = JObject.Parse(json);
+            ConfigFileData cfd = new ConfigFileData();
+            cfd.OutputDir = (string)jsonObj["outputDir"];
+            cfd.SourceDir = (string)jsonObj["sourceDir"];
+            cfd.LogName = (string)jsonObj["logName"];
+            cfd.ThumnailSize = (string)jsonObj["thumnailSize"];
+            JArray inputDirs = new JArray(jsonObj["outputDir"]);
+            cfd.InputDirs = inputDirs.Select(jv => (string)jv).ToArray();
+
+            return cfd;
+        }
+    }
+}
