ad89505043dded287f738d884223095469a25174
Yuval Weinstein <yuvalw11@Yuvals-MacBook-Pro.local>
2018-06-06 00:25:21 +0300

    Adding Web Application Project

---
+++ WebApplication2/WebApplication2/Controllers/ImageWebViewController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ImageWebViewController : Controller
+    {
+        ImageWebModel model;
+
+        public ImageWebViewController()
+        {
+            model = new ImageWebModel();
+        }
+
+        [HttpGet]
+        public ActionResult ImageWebView()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JObject GetServiceData()
+        {
+            JObject data = new JObject();
+            data["status"] = this.model.GetServiceStatus();
+            data["imageNum"] = this.model.GetPhotosNumber();
+            return data;
+        }
+
+        [HttpGet]
+        public JObject GetStudentsData()
+        {
+            JObject data = new JObject();
+            JObject stu1 = new JObject();
+            JObject stu2 = new JObject();
+            JArray arr = new JArray();
+            stu1["id"] = this.model.GetStudentData("student1", "id");
+            stu1["firstName"] = this.model.GetStudentData("student1", "firstName");
+            stu1["lastName"] = this.model.GetStudentData("student1", "lastName");
+            stu2["id"] = this.model.GetStudentData("student2", "id");
+            stu2["firstName"] = this.model.GetStudentData("student2", "firstName");
+            stu2["lastName"] = this.model.GetStudentData("student2", "lastName");
+            arr.Add(stu1);
+            arr.Add(stu2);
+            data["students"] = arr;
+            return data;
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/ImageWebModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace WebApplication2.Models
+{ 
+
+    public class ImageWebModel
+    {
+        public string GetStudentData(string stu, string field)
+        {
+            var path = HttpContext.Current.Server.MapPath(@"..\\App_Data\\StudentsData.xml");
+            XDocument doc = XDocument.Load(path);
+            var students = doc.Element("students");
+            var student = students.Element(stu);
+            return student.Element(field).Value;
+        }
+
+        public int GetPhotosNumber()
+        {
+            return 0;
+        }
+
+        public string GetServiceStatus()
+        {
+            return "Inactive";
+        }
+    }
+}
